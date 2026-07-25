d1301a033d107f5522641c729286ec2d7ce3c24b
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-07-25 19:15:20 +0600

    comment's get,post,update & delete added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+const { randomUUID } = require("crypto");
+
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -30,11 +32,36 @@ async function run() {
 
     const db = client.db("ideavolt");
     const ideaCollection = db.collection("ideas");
+    const myIdeaCollection = db.collection("my-ideas");
 
-    app.get("/ideas", async (req, res) => {
-      const result = await ideaCollection.find().toArray();
-      res.send(result);
-    });
+    // app.get("/ideas", async (req, res) => {
+    //   const result = await ideaCollection.find().toArray();
+    //   res.send(result);
+    // });
+    
+
+app.get("/ideas", async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) ;
+
+    const pipeline = [
+      {$sort: {_id:-1} }
+    ]
+    if(!isNaN(limit)){
+      pipeline.push({$limit:limit})
+    }
+
+    const result = await ideaCollection
+      .aggregate(pipeline)
+      .toArray();
+
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+
 
     app.post("/ideas", async (req, res) => {
       const ideaData = req.body;
@@ -43,6 +70,110 @@ async function run() {
 
       res.json(result);
     });
+
+    app.get("/ideas/:id", async (req, res) => {
+      const { id } = req.params;
+
+      const result = await ideaCollection.findOne({ _id: new ObjectId(id) });
+
+      res.send(result);
+    });
+
+// comment get,add, update, delete added start
+app.get("/ideas/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const idea = await ideaCollection.findOne(
+      { _id: new ObjectId(id) },
+      { projection: { comments: 1 } }
+    );
+
+    res.send(idea?.comments || []);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+//post add
+app.post("/ideas/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const { userName, photoURL, text } = req.body;
+
+    const comment = {
+      _id: randomUUID(),
+      userName,
+      photoURL: photoURL || "",
+      text,
+      createdAt: new Date(),
+    };
+
+    await ideaCollection.updateOne(
+      { _id: new ObjectId(id) },
+      {
+        $push: {
+          comments: comment,
+        },
+      }
+    );
+
+    res.send(comment);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+//update
+app.patch("/ideas/:ideaId/comments/:commentId", async (req, res) => {
+  try {
+    const { ideaId, commentId } = req.params;
+
+    const { text } = req.body;
+
+    await ideaCollection.updateOne(
+      {
+        _id: new ObjectId(ideaId),
+        "comments._id": commentId,
+      },
+      {
+        $set: {
+          "comments.$.text": text,
+        },
+      }
+    );
+
+    res.send({ success: true });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+//delete
+app.delete("/ideas/:ideaId/comments/:commentId", async (req, res) => {
+  try {
+    const { ideaId, commentId } = req.params;
+
+    await ideaCollection.updateOne(
+      { _id: new ObjectId(ideaId) },
+      {
+        $pull: {
+          comments: {
+            _id: commentId,
+          },
+        },
+      }
+    );
+
+    res.send({ success: true });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+
+// comment get,add, update, delete added end
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
